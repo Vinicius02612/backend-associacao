986c7299bda2c13c3f1dec6a103604b56fff0ceb
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-11-27 18:28:24 -0300

    Ajustando erros de acesso aos schemas e corrigindo retornos

--- src/routers/router_despesas.py
+++ src/routers/router_despesas.py
@@ -12,6 +12,8 @@ router = APIRouter(prefix="/despesas")
 @router.get("/", response_model=List[DespesaResponse])
 def get_despesas(db:Session = Depends(get_db)) -> List[Despesas]:
     despesas = db.query(Despesas).all()
+    if not despesas:
+        raise HTTPException(status_code=404, detail="Não há despesas cadastradas")
     return despesas
 
 @router.post("/", response_model=DespesaResponse, status_code=201)
@@ -23,14 +25,14 @@ def post_despesas(despesa: DespesaRequest, db: Session = Depends(get_db)) -> Des
     db.commit()
     db.refresh(new_despesa)
 
-    return Despesas(**new_despesa.model_dump())
+    return new_despesa
 
 @router.put("/{id}", response_model=DespesaResponse)
-def despesas_update(id:int, despesa: DespesaRequest, db: Session = Depends(get_db)) -> Despesas:
+def despesas_update(id:int, despesa_request: DespesaRequest, db: Session = Depends(get_db)) -> Despesas:
     despesa = db.query(Despesas).filter(Despesas.id == id).first()
     if not despesa:
         raise HTTPException(status_code=404, detail="Despesa não encontrada")
-    for key, value in despesa.dict().items():
+    for key, value in despesa_request.model_dump().items():
         setattr(despesa, key, value)
    
     db.commit()
