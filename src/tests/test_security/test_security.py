b168193657d963bece2533e7e5fd1d6755c421fc
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-11-27 23:55:39 -0300

    criando testes token acess

---
+++ src/tests/test_security/test_security.py
@@ -0,0 +1,10 @@
+from jwt import decode
+from src.services.security import create_access_token, SECRETY_KEY,ALGORITHM
+
+
+def test_create_access_token():
+    data = {'test': 'test'}
+    token = create_access_token(data)
+    decoded = decode(token, SECRETY_KEY, algorithms=[ALGORITHM])
+    assert decoded['test'] == 'test'
+    assert decoded['exp']
